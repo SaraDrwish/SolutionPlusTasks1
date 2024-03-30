c361020bcc35a63ff99cd1a3e4fdea3298590c1f
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-03-30 22:50:34 +0300

    other todo first design v1

--- crud-tsk4-todo/src/views/TodoView.vue
+++ crud-tsk4-todo/src/views/TodoView.vue
@@ -9,33 +9,42 @@
               <div
                 class="flex gap-4 bg-slate-700 p-4 my-9 justify-evenly rounded-[1rem]"
               >
-                <input
-                  type="text"
-                  class="w-[80%] rounded-[1rem] p-2 outline-none"
-                />
-                <input
-                  class="cursor-pointer text-red-400 border-2 border-red-400 p-2"
-                  type="button"
-                  value="Add"
-                />
+                <form @submit.prevent="">
+                  <div class="flex gap-2 w-[90%]">
+                    <input
+                      type="text"
+                      class="w-[80%] rounded-[1rem] p-2 outline-none flex-1"
+                      placeholder="write your title here ... "
+                    />
+                    <input
+                      type="text"
+                      class="w-[80%] rounded-[1rem] p-2 outline-none flex-1"
+                      placeholder="write your description here ... "
+                    />
+                  </div>
+                  <input
+                    class="cursor-pointer text-red-400 border-2 border-red-400 p-2"
+                    type="button"
+                    value="Add"
+                  />
+                </form>
               </div>
             </li>
           </ul>
         </div>
         <div class="">
-          <h3></h3>
-          <div class="bg-green-100 p-1 w-[100%] mx-auto rounded-[1rem]">
-            <ul>
+          <div class="bg-green-100 p-1 w-[90%] mx-auto rounded-[1rem]">
+            <ul v-for="i in 4" :key="i.id">
               <li>
                 <div
-                  class="flex gap-4 w-[80%] bg-slate-300 p-2 my-4 mx-auto justify-evenly rounded-[1rem]"
+                  class="flex gap-4 w-[90%] bg-[#a7b2b5] p-2 my-4 mx-auto justify-evenly rounded-[1rem]"
                 >
                   <input
                     type="checkbox"
                     class="p-2 m-1 outline-none text-orange-400 bg-orange-200"
                   />
                   <div
-                    class="flex gap-1 w-[80%] bg-gray-600 my-1 mx-auto text-orange-600 p-4 rounded-[1rem]"
+                    class="flex gap-1 w-[80%] bg-slate-700 my-1 mx-auto text-orange-600 p-4 rounded-[1rem]"
                   >
                     <input
                       type="text"
@@ -77,10 +86,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$base-color: #a7b2b5;
 .todo {
   text-transform: capitalize;
   width: 90%;
-  background: rgb(167, 178, 181);
+  background: $base-color;
   padding: 1rem;
   margin: auto;
   border-radius: 2rem;
